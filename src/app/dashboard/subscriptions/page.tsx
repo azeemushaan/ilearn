8ccaae69a4ce356a9f3d3d37b26e3a7027716704
Azeem Ushan <azeemushan4@gmail.com>
2025-10-27 18:24:42 +0000

    Try fixing this error: `Runtime Error: Error: React is not defined. Erro

--- src/app/dashboard/subscriptions/page.tsx
+++ src/app/dashboard/subscriptions/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import React from "react";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { PlusCircle } from "lucide-react";
